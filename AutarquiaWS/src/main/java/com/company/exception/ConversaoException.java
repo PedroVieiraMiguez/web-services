9607ac08c80f1ddd89877aafc101d9c0b3d6b9a6
Pedro Miguez <pedrovieiramiguez@upskill.pt>
2021-01-19 17:05:29 +0000

    AutarquiaWS start

---
+++ AutarquiaWS/src/main/java/com/company/exception/ConversaoException.java
@@ -0,0 +1,9 @@
+package com.company.exception;
+
+/**
+ *
+ * @author pedro
+ */
+public class ConversaoException {
+
+}
---
+++ AutarquiaWS/src/main/java/com/company/model/Data.java
@@ -0,0 +1,9 @@
+package com.company.model;
+
+/**
+ *
+ * @author pedro
+ */
+public class Data {
+
+}
